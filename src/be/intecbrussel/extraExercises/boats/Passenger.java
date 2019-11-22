0a7173c097368e26f8dcd62d2b5059933a362dba
Calhoun VP <cal.van.paesschen@gmail.com>
2019-11-22 14:03:20 +0100

    finished passenger

--- src/be/intecbrussel/extraExercises/boats/Passenger.java
+++ src/be/intecbrussel/extraExercises/boats/Passenger.java
@@ -1,4 +1,36 @@
 package be.intecbrussel.extraExercises.boats;
 
 public class Passenger extends Person  {
+
+    //properties
+    private String destination;
+
+    Passenger() {
+        super();
+    }
+
+    Passenger(String name) {
+        super(name);
+    }
+
+    Passenger(String name, int age) {
+        super(name, age);
+    }
+
+    Passenger(String name, int age, String destination) {
+        super(name, age);
+        this.destination = destination;
+    }
+
+    //setter
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    //getter
+    public String getDestination() {
+        return destination;
+    }
+
+
 }
